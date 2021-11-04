0d32e9af995104657e4f6338f33931612bcefcc8
임규혁 <ykh8383633@naver.com>
2021-11-04 20:00:15 +0900

    first commit

---
+++ src/main/java/app/joycourse/www/prod/JoyCourseApplication.java
@@ -0,0 +1,13 @@
+package app.joycourse.www.prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/joycourse/www/prod/JoyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package app.joycourse.www.prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
