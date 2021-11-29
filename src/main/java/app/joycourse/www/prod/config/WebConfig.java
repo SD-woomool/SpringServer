17b3daba9c33ef161ab3093b50aa134a959665b8
seokh1213 <seokh1213@gmail.com>
2021-11-29 13:48:11 +0900

    feature: change jackson naming-startegysnake case

--- src/main/java/app/joycourse/www/prod/config/WebConfig.java
+++ src/main/java/app/joycourse/www/prod/config/WebConfig.java
@@ -1,6 +1,10 @@
 package app.joycourse.www.prod.config;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -11,5 +15,18 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter(jackson2ObjectMapperBuilder().build()));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder()
+                .failOnUnknownProperties(false) // SpringBoot default
+                .featuresToDisable(MapperFeature.DEFAULT_VIEW_INCLUSION) // SpringBoot default
+                .featuresToEnable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // SpringBoot default
+                .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
 
 }
