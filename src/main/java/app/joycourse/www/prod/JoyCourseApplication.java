7b498a30c54b82f9ec2ee1ab138970e9121e1749
홍석현 <seokh1213@gmail.com>
2022-01-30 00:09:31 +0900

    feature: test data 추가하기

--- src/main/java/app/joycourse/www/prod/JoyCourseApplication.java
+++ src/main/java/app/joycourse/www/prod/JoyCourseApplication.java
@@ -1,36 +1,51 @@
 package app.joycourse.www.prod;
 
+import app.joycourse.www.prod.domain.User;
+import app.joycourse.www.prod.repository.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import javax.persistence.EntityManager;
-
 @ServletComponentScan
 @SpringBootApplication
 @EnableWebMvc
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
 public class JoyCourseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JoyCourseApplication.class, args);
-	}
-	@Bean
-	DispatcherServlet dispatcherServlet () {  // 404에러는 컨트롤러 까지 오지 않고 dispatchServlet에서 해결됨, 그걸 해결하기 위해
-		DispatcherServlet ds = new DispatcherServlet();
-		ds.setThrowExceptionIfNoHandlerFound(true);
-		return ds;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JoyCourseApplication.class, args);
+    }
 
+    @Bean
+    DispatcherServlet dispatcherServlet() {  // 404에러는 컨트롤러 까지 오지 않고 dispatchServlet에서 해결됨, 그걸 해결하기 위해
+        DispatcherServlet ds = new DispatcherServlet();
+        ds.setThrowExceptionIfNoHandlerFound(true);
+        return ds;
+    }
 
+    @Bean
+    CommandLineRunner insertTestData(AccountRepository accountRepository) {
+        return args -> {
+            User user1 = new User();
+            user1.setNickname("ykh");
+            user1.setEmail("ykh@email.com");
+            user1.setAgeRange(20);
+            user1.setGender(1);
+            user1.setCreateAt();
+            accountRepository.newUser(user1);
 
+            User user2 = new User();
+            user2.setNickname("hsh");
+            user2.setEmail("hsh@email.com");
+            user2.setAgeRange(20);
+            user2.setGender(1);
+            user2.setCreateAt();
+            accountRepository.newUser(user2);
 
+            System.out.println("Test user(ykh, hsh) is created");
+        };
+    }
 }
--- src/main/java/app/joycourse/www/prod/repository/JpaAccountRepository.java
+++ src/main/java/app/joycourse/www/prod/repository/JpaAccountRepository.java
@@ -1,22 +1,23 @@
 package app.joycourse.www.prod.repository;
 
 import app.joycourse.www.prod.domain.User;
-
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class JpaAccountRepository implements AccountRepository{
+@Transactional
+public class JpaAccountRepository implements AccountRepository {
     private final EntityManager em;
 
-    public JpaAccountRepository(EntityManager em){
+    public JpaAccountRepository(EntityManager em) {
         this.em = em;
     }
 
-    public EntityManager getEm(){
+    public EntityManager getEm() {
         return this.em;
     }
 
@@ -29,35 +30,35 @@ public class JpaAccountRepository implements AccountRepository{
     }
 
     @Override
-    public Optional<User> findByNickname(String nickname){
+    public Optional<User> findByNickname(String nickname) {
         List<User> result = em.createQuery("select m from User m where m.nickname = :nickname", User.class).
                 setParameter("nickname", nickname).getResultList();
         return result.stream().findAny();
     }
 
     @Override
-    public Optional<User> findById(Long id){ // 이거다시 보자
+    public Optional<User> findById(Long id) { // 이거다시 보자
         return Optional.ofNullable(em.find(User.class, id));
     }
 
     @Override
-    public User newUser(User user){
+    public User newUser(User user) {
         em.persist(user); // 한번 찾아보자
         return user;
     }
 
     @Override
-    public void deleteUser(User user){
+    public void deleteUser(User user) {
         Long id = user.getId();
         int deleteCount = em.createQuery("delete from User u where u.id = :id").
                 setParameter("id", id).executeUpdate();
-        System.out.println("삭제한 갯수: "+deleteCount);
+        System.out.println("삭제한 갯수: " + deleteCount);
     }
 
     @Override
-    public void updateUser(User user, User userInfo){
+    public void updateUser(User user, User userInfo) {
         int resultCount = em.createQuery("update User u set u.profileImageUrl = :profileImageUrl, u.gender = :gender, u.ageRange = :ageRange " +
-                "where u.id = :id").
+                        "where u.id = :id").
                 setParameter("profileImageUrl", userInfo.getProfileImageUrl()).
                 setParameter("gender", userInfo.getGender()).
                 setParameter("ageRange", userInfo.getAgeRange()).
