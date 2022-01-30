919047c972554875c2b6820d8de3322ca0983b14
seokh1213 <seokh1213@gmail.com>
2022-01-30 15:14:31 +0900

    fix: main import 안된거 수정

--- src/main/java/app/joycourse/www/prod/JoyCourseApplication.java
+++ src/main/java/app/joycourse/www/prod/JoyCourseApplication.java
@@ -6,23 +6,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import javax.transaction.Transactional;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableWebMvc
 public class JoyCourseApplication {
-  
+
     public static void main(String[] args) {
         SpringApplication.run(JoyCourseApplication.class, args);
-    }
-
-    @Bean
-    DispatcherServlet dispatcherServlet() {  // 404에러는 컨트롤러 까지 오지 않고 dispatchServlet에서 해결됨, 그걸 해결하기 위해
-        DispatcherServlet ds = new DispatcherServlet();
-        ds.setThrowExceptionIfNoHandlerFound(true);
-        return ds;
     }
 
     @Bean
